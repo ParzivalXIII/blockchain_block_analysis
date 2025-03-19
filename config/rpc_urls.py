e5885e3d4482b2946f025658978ba1e4f816dc37
ParzivalXIII <kayodeibrahim97@gmail.com>
2025-03-19 06:52:14 -0400

    Update rpc_urls.py

--- config/rpc_urls.py
+++ config/rpc_urls.py
@@ -1,4 +1,4 @@
 RPC_URLS = {
-    "ethereum": "https://mainnet.infura.io/v3/cad361a69a9f4783a54b769f20e4dafa",  # Replace with your Infura key
-    "polygon": "https://polygon-rpc.com/"
+    "ethereum": "https://mainnet.infura.io/v3/YOUR_API_KEY",  # Replace with your Infura key
+    "polygon": "https://polygonscan.com/"
 }
